507badbc9c8d7acea90b4577f6b48c9ba5a0d68e
Aesothoth <90930838+Aesothoth@users.noreply.github.com>
2023-07-14 01:55:36 -0400

    Update GravityOrbit.cs

--- GravityOrbit.cs
+++ GravityOrbit.cs
@@ -7,11 +7,21 @@ public class GravityOrbit : MonoBehaviour
 
     public float gravity;
     public bool fixedDirection;
+    public bool secondGravity = false;
     // Start is called before the first frame update
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.GetComponent<GravityControl>())
+        if(other.GetComponent<DoubleGravityControl>())
+        {
+            if(other.GetComponent<DoubleGravityControl>().Gravity1 == null)
+            {
+                other.GetComponent<DoubleGravityControl>().Gravity1 = this.GetComponent<GravityOrbit>();
+            }else if(other.GetComponent<DoubleGravityControl>().Gravity1 != null && other.GetComponent<DoubleGravityControl>().Gravity2 != this.gameObject){
+                other.GetComponent<DoubleGravityControl>().Gravity2 = this.GetComponent<GravityOrbit>();
+                secondGravity = true;
+            }
+        }else if(other.GetComponent<GravityControl>())
         {
             other.GetComponent<GravityControl>().Gravity = this.GetComponent<GravityOrbit>();
         }
@@ -19,10 +29,17 @@ public class GravityOrbit : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.GetComponent<GravityControl>())
+        if(other.GetComponent<DoubleGravityControl>() &! secondGravity)
+        {
+            other.GetComponent<DoubleGravityControl>().Gravity1 = null;
+
+        }else if(other.GetComponent<DoubleGravityControl>() && secondGravity)
+        {
+            other.GetComponent<DoubleGravityControl>().Gravity2 = null;
+        }
+        else if(other.GetComponent<GravityControl>())
         {
             other.GetComponent<GravityControl>().Gravity = null;
         }
     }
-
 }
